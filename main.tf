db526d9da7525043dd88648e81c7d123a61cb32f
zerai <teclaizerai@gmail.com>
2022-01-13 21:02:30 +0100

    Added dorplet resource.

--- main.tf
+++ main.tf
@@ -2,3 +2,17 @@ terraform {
   required_version = "~> 1.0.2"
 
 }
+
+
+resource "digitalocean_droplet" "tf-backend-prova" {
+  image = "ubuntu-20-04-x64"
+  name = "tf-backend-prova"
+  region = "fra1"
+  #size = "s-2vcpu-4gb"
+  size     = "s-1vcpu-1gb"
+  #private_networking = true
+  tags   = ["terraform-backent-remote-testing"]
+  #count  = "1"
+  graceful_shutdown = false
+  backups = false
+}
